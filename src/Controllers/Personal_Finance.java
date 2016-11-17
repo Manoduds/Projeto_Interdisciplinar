0a99f049ab981181a8641b4e308c6e2ecb1ca2fb
aluno <aluno@aluno-PC>
2016-11-17 20:35:41 -0200

    Criação de scenes, com devidas alterações na Main.java.

--- src/Controllers/Personal_Finance.java
+++ src/Controllers/Personal_Finance.java
@@ -5,9 +5,17 @@
  */
 package Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,6 +30,17 @@ public class Personal_Finance implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }  
+    
+    @FXML
+    private void BtnMain(ActionEvent event) throws IOException 
+    {
+        Parent Login_Parent = FXMLLoader.load(getClass().getResource("/FXML/Main.fxml"));        
+        Scene scene = new Scene(Login_Parent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(scene);
+        app_stage.centerOnScreen();
+        app_stage.show();        
+    }
     
 }
